afdc07c50010195604fa2a8ddc02527f5cf4412b
Orkhan <gasanov.orkhan1@gmail.com>
2025-09-14 01:37:26 -0700

    [Tutorial 4]

--- index.js
+++ index.js
@@ -27,8 +27,16 @@ const typeDefs = gql`
     author: String!
   }
 
+  input UpdateBookInput {
+    id: ID!
+    title: String
+    author: String
+  }
+
   type Mutation {
     addBook(input: BookInput!): Book!
+    updateBook(input: UpdateBookInput!): Book
+    deleteBook(id: ID!): Boolean!
   }
 `;
 
@@ -52,17 +60,35 @@ const resolvers = {
         "INSERT INTO books (title, author) VALUES (?, ?)",
         [title, author]
       );
+      return { id: result.insertId, title, author };
+    },
 
-      // Return the newly created book
-      return {
-        id: result.insertId,
-        title,
-        author
-      };
+    updateBook: async (_, { input }) => {
+      const { id, title, author } = input;
+
+      // Fetch current book
+      const [existing] = await pool.query("SELECT * FROM books WHERE id = ?", [id]);
+      if (existing.length === 0) return null;
+
+      // Update fields (only if provided)
+      const newTitle = title || existing[0].title;
+      const newAuthor = author || existing[0].author;
+
+      await pool.query("UPDATE books SET title = ?, author = ? WHERE id = ?", [
+        newTitle,
+        newAuthor,
+        id
+      ]);
+
+      return { id, title: newTitle, author: newAuthor };
+    },
+
+    deleteBook: async (_, { id }) => {
+      const [result] = await pool.query("DELETE FROM books WHERE id = ?", [id]);
+      return result.affectedRows > 0;
     }
   }
 };
-
 
 // Start Apollo Server
 const server = new ApolloServer({ typeDefs, resolvers });
